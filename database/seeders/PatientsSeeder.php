34318a64a27d96dfea20d6a241f4ce672313a1b2
patricknuel <202110210@gordoncollege.edu.ph>
2024-04-12 00:52:11 +0800

    pat commit(1)

--- database/seeders/PatientsSeeder.php
+++ database/seeders/PatientsSeeder.php
@@ -5,14 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class PatientSeeder extends Seeder
+class PatientsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
-        // \App\Models\Patient::factory(20)->create();
+        \App\Models\Patients::factory(10)->create();
     }
 }
